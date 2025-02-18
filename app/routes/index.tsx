397cecf5a12a4d68d048ee1611f452518ab4c4ca
DanSnow <dododavid006@gmail.com>
2025-02-18 21:27:56 +0800

    feat: add docker and moonrepo

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,4 +1,6 @@
 import { createFileRoute } from '@tanstack/react-router'
+import { createServerFn } from '@tanstack/start'
+import { useCallback } from 'react'
 import { Button } from '~/components/ui/button'
 
 export const Route = createFileRoute('/')({
@@ -8,12 +10,22 @@ export const Route = createFileRoute('/')({
   },
 })
 
+const hello = createServerFn({
+  method: 'POST',
+}).handler(() => {
+  return 'Hello World!'
+})
+
 function Home() {
   const data = Route.useLoaderData()
+  const handleClick = useCallback(async () => {
+    const res = await hello()
+    console.log(res)
+  }, [])
   return (
     <div className="p-2">
       <h3>{data}</h3>
-      <Button>Click Me!</Button>
+      <Button onClick={handleClick}>Click Me!</Button>
     </div>
   )
 }
