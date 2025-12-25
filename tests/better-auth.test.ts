ccac24cbbb0ea1c453c108b93a417b60a04b3de5
DanSnow <dododavid006@gmail.com>
2025-12-25 16:10:00 +0800

    test: fix test

--- tests/better-auth.test.ts
+++ tests/better-auth.test.ts
@@ -1,4 +1,4 @@
-import { fileURLToPath } from 'node:url';
+import { fileURLToPath, pathToFileURL } from 'node:url';
 import { expect, it } from 'vitest';
 import { $, fs, path } from 'zx';
 
@@ -8,6 +8,15 @@ it('should generate auth schema', { timeout: 60_000 }, async () => {
   await fs.rm(path.resolve(__dirname, '../src/drizzle/auth-schema.ts'), {
     force: true,
   });
+
+  if (process.env.CI) {
+    if (!process.env.DB_FILE_NAME) {
+      process.env.DB_FILE_NAME = pathToFileURL(path.join(__dirname, 'test.sqlite')).toString();
+    }
+    if (!process.env.HASH_ID_SECRET) {
+      process.env.HASH_ID_SECRET = 'secret';
+    }
+  }
 
   await $({ input: 'y' })`pnpm exec moon run generate-auth`;
 
