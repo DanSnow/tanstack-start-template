c9535c941ea4d25230716101e746ca933522efe4
DanSnow <dododavid006@gmail.com>
2026-04-06 18:45:46 +0800

    fix(test): add missing env vars to .env.test

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,4 +1,8 @@
 import { defineConfig } from 'vitest/config';
+import { loadEnv } from 'vite';
+import process from 'node:process';
+
+Object.assign(process.env, loadEnv('test', process.cwd(), ''));
 
 export default defineConfig({
   test: {
