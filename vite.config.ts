6d936ca3a70bd16a2469a2ba436f8fefd0783f7e
DanSnow <dododavid006@gmail.com>
2026-04-22 01:28:00 +0800

    chore: add nitro and upgrade deps

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,10 @@ import tailwindcss from '@tailwindcss/vite';
 import { devtools } from '@tanstack/devtools-vite';
 import { tanstackStart } from '@tanstack/react-start/plugin/vite';
 import React from '@vitejs/plugin-react';
+import { nitro } from 'nitro/vite';
 import { defineConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
-  plugins: [devtools(), tsconfigPaths(), tailwindcss(), tanstackStart({}), React()],
+  plugins: [devtools(), tsconfigPaths(), tailwindcss(), tanstackStart({}), React(), nitro({ preset: 'node-server' })],
 });
