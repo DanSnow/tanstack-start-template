c845f61534107ed6c6d15eab250e40271fc64c99
DanSnow <dododavid006@gmail.com>
2026-06-04 01:47:22 +0800

    chore: update

--- vite.config.ts
+++ vite.config.ts
@@ -4,10 +4,12 @@ import { tanstackStart } from '@tanstack/react-start/plugin/vite';
 import React from '@vitejs/plugin-react';
 import { nitro } from 'nitro/vite';
 import { defineConfig } from 'vite';
-import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
-  plugins: [devtools(), tsconfigPaths(), tailwindcss(), tanstackStart({}), React(), nitro({ preset: 'node-server' })],
+  resolve: {
+    tsconfigPaths: true,
+  },
+  plugins: [devtools(), tailwindcss(), tanstackStart({}), React(), nitro({ preset: 'node-server' })],
   optimizeDeps: {
     exclude: ['@tanstack/devtools'],
   },
